a91a28ca6961da91d1c13ad1fcdb266150b40e5a
kendall n <kdef2012@gmail.com>
2025-12-02 04:34:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "owl-about-that-9f67d",
+  "appId": "1:776579734405:web:d23fe092351a264222c2ec",
+  "apiKey": "AIzaSyAb-ujSqho9tRhJljjY1t1qtjQaqsCRKnA",
+  "authDomain": "owl-about-that-9f67d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "776579734405"
+};
