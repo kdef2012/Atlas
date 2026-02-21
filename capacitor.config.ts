9d589aff3a1cc9573fe097508fdd8add166267c9
kendall n <kdef2012@gmail.com>
2026-02-21 04:23:44 +0000

    npx cap sync android ⠹ copy android [warn] Cannot copy web assets from o

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,14 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.playatlas.nebula',
   appName: 'ATLAS',
-  webDir: 'out',
+  webDir: 'public', // Using 'public' instead of 'out' because it always exists in NextJS
   server: {
     // Points the native app to your live production signal
     url: 'https://studio--owl-about-that-9f67d.us-central1.hosted.app/',
     cleartext: true
+  },
+  android: {
+    allowMixedContent: true
   }
 };
 
